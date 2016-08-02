ee18ab3053e1862c03082b79ffab682be288f4c1
Alessandro Tegner <alessandro.tegner@gmail.com>
2016-08-02 01:15:08 -0300

    First commit

---
+++ config.rb
@@ -0,0 +1,55 @@
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+# General configuration
+
+set :build_dir, 'alesshh.github.io'
+
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+###
+# Helpers
+###
+
+# Methods defined in the helpers block are available in templates
+helpers do
+  def page_title
+    current_page.data.title || data.site.title
+  end
+
+  def page_description
+    current_page.data.description || data.site.description
+  end
+
+  def page_image
+    current_page.data.image || url_for('me.svg')
+  end
+end
+
+# Build-specific configuration
+configure :build do
+  # Minify CSS on build
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+end
